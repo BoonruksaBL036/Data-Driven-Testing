b372b06a50f0e20407b7772f3612f0d1e99087e4
BoonruksaBL036 <664259036@webmail.npru.ac.th>
2025-04-01 23:12:12 +0700

    success

--- src/test/java/TestRegister.java
+++ src/test/java/TestRegister.java
@@ -1,83 +1,110 @@
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestRegister {
     @Test
-    void test101() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "./chromedriver/chromedriver-win32/chromedriver.exe");
+    void test01() throws IOException {
+        System.setProperty("webdriver.chrome.driver", "./chromedriver/chromedriver.exe");
 
         String path = "./Excel/Register.xlsx";
         FileInputStream fs = new FileInputStream(path);
+
         XSSFWorkbook workbook = new XSSFWorkbook(fs);
         XSSFSheet sheet = workbook.getSheetAt(0);
         int row = sheet.getLastRowNum() + 1;
 
-        for (int i = 1; i < row; i++) {
+        for(int i=1; i<row-1; i++) {
             WebDriver driver = new ChromeDriver();
-            driver.get("https://sc.npru.ac.th/sc_shortcourses/signup");
+            driver.get("http://localhost/sc_shortcourses/signup");
 
-            Row rows = sheet.getRow(i);
-            if (rows == null) continue;
+            String nameTitleTha = sheet.getRow(i).getCell(1).toString();
 
-            String nameTitleTha = (rows.getCell(1) != null) ? rows.getCell(1).toString() : "";
-            String firstnameTha = (rows.getCell(2) != null) ? rows.getCell(2).toString() : "";
-            String lastnameTha = (rows.getCell(3) != null) ? rows.getCell(3).toString() : "";
-            String nameTitleEng = (rows.getCell(4) != null) ? rows.getCell(4).toString() : "";
-            String firstnameEng = (rows.getCell(5) != null) ? rows.getCell(5).toString() : "";
-            String lastnameEng = (rows.getCell(6) != null) ? rows.getCell(6).toString() : "";
-            String birthDate = (rows.getCell(7) != null) ? String.valueOf((int) rows.getCell(7).getNumericCellValue()) : "";
-            String birthMonth = (rows.getCell(8) != null) ? String.valueOf((int) rows.getCell(8).getNumericCellValue()) : "";
-            String birthYear = (rows.getCell(9) != null) ? String.valueOf((int) rows.getCell(9).getNumericCellValue()) : "";
-            String idCard = (rows.getCell(10) != null) ? rows.getCell(10).toString() : "";
-            String password = (rows.getCell(11) != null) ? rows.getCell(11).toString() : "";
-            String mobile = (rows.getCell(12) != null) ? rows.getCell(12).toString() : "";
-            String email = (rows.getCell(13) != null) ? rows.getCell(13).toString() : "";
-            String address = (rows.getCell(14) != null) ? rows.getCell(14).toString() : "";
-            String province = (rows.getCell(15) != null) ? rows.getCell(15).toString() : "";
-            String district = (rows.getCell(16) != null) ? rows.getCell(16).toString() : "";
-            String subDistrict = (rows.getCell(17) != null) ? rows.getCell(17).toString() : "";
-            String postalCode = (rows.getCell(18) != null) ? String.valueOf((int) rows.getCell(18).getNumericCellValue()) : "";
+            XSSFRow currentRow = sheet.getRow(i);
+            if (currentRow == null || nameTitleTha.isEmpty()) {
+                System.out.println("Test successfully");
+                driver.quit();
+                break;
+            }
 
-            System.out.println("birthMonth" + birthMonth);
-            new Select(driver.findElement(By.id("nameTitleTha"))).selectByVisibleText(nameTitleTha);
+            new Select(driver.findElement(By.id("nameTitleTha"))).selectByValue(nameTitleTha);
+
+            String firstnameTha = sheet.getRow(i).getCell(2).toString();
             driver.findElement(By.id("firstnameTha")).sendKeys(firstnameTha);
+
+            String lastnameTha = sheet.getRow(i).getCell(3).toString();
             driver.findElement(By.id("lastnameTha")).sendKeys(lastnameTha);
-            new Select(driver.findElement(By.id("nameTitleEng"))).selectByVisibleText(nameTitleEng);
+
+            String nameTitleEng = sheet.getRow(i).getCell(4).toString();
+            new Select(driver.findElement(By.id("nameTitleEng"))).selectByValue(nameTitleEng);
+
+            String firstnameEng = sheet.getRow(i).getCell(5).toString();
             driver.findElement(By.id("firstnameEng")).sendKeys(firstnameEng);
+
+            String lastnameEng = sheet.getRow(i).getCell(6).toString();
             driver.findElement(By.id("lastnameEng")).sendKeys(lastnameEng);
-            new Select(driver.findElement(By.id("birthDate"))).selectByVisibleText(birthDate);
+
+            String birthDate = sheet.getRow(i).getCell(7).toString();
+            new Select(driver.findElement(By.id("birthDate"))).selectByValue(birthDate);
+
+            String birthMonth = sheet.getRow(i).getCell(8).toString();
             new Select(driver.findElement(By.id("birthMonth"))).selectByValue(birthMonth);
+
+            String birthYear = sheet.getRow(i).getCell(9).toString();
             new Select(driver.findElement(By.id("birthYear"))).selectByValue(birthYear);
+
+            String idCard = sheet.getRow(i).getCell(10).toString();
             driver.findElement(By.id("idCard")).sendKeys(idCard);
+
+            String password = sheet.getRow(i).getCell(11).toString();
             driver.findElement(By.id("password")).sendKeys(password);
+
+            String mobile = sheet.getRow(i).getCell(12).toString();
             driver.findElement(By.id("mobile")).sendKeys(mobile);
+
+            String email = sheet.getRow(i).getCell(13).toString();
             driver.findElement(By.id("email")).sendKeys(email);
+
+            String address = sheet.getRow(i).getCell(14).toString();
             driver.findElement(By.id("address")).sendKeys(address);
-            driver.findElement(By.id("province")).sendKeys(province);
+
+            String province = sheet.getRow(i).getCell(15).toString();
+            new Select(driver.findElement(By.id("province"))).selectByValue(province );
+
+            String district = sheet.getRow(i).getCell(16).toString();
             driver.findElement(By.id("district")).sendKeys(district);
+
+            String subDistrict = sheet.getRow(i).getCell(17).toString();
             driver.findElement(By.id("subDistrict")).sendKeys(subDistrict);
+
+            String  postalCode = sheet.getRow(i).getCell(18).toString();
             driver.findElement(By.id("postalCode")).sendKeys(postalCode);
 
+//            String  acceptExcel = sheet.getRow(i).getCell(19).toString();
             WebElement accept = driver.findElement(By.id("accept"));
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            if (!accept.isSelected()){
-                js.executeScript("arguments[0].click();",accept);
-            }
-//            driver.quit();
-        }
+            WebElement submitBtn = driver.findElement(By.xpath("/html/body/section/div/div/form/div[6]/button"));
+            if (!accept.isSelected()) {
 
-        workbook.close();
-        fs.close();
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                if(!accept.isSelected()){
+                    js.executeScript("arguments[0].click();", accept);
+                    js.executeScript("arguments[0].click();", submitBtn);
+                }
+            }
+
+            WebElement alertTitle = driver.findElement(By.id("swal2-title"));
+            assertEquals("ลงทะเบียนสำเร็จ", alertTitle.getText());
+
+            driver.quit();
+        }
     }
 }
